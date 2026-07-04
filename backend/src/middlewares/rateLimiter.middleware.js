63281945b94ce7898e4ccb51397521c158843cdf
Hemant <hemantyadav5939@gmail.com>
2026-07-04 19:04:26 +0530

    increased request limit to 1000 for rate limiter middleware

--- backend/src/middlewares/rateLimiter.middleware.js
+++ backend/src/middlewares/rateLimiter.middleware.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100,                 // limit each IP to 100 requests per window
+    max: 1000,                 // limit each IP to 1000 requests per window
     standardHeaders: true,
     legacyHeaders: false,
     message: {
